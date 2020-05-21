d46e0034cde357605df714aa01fd99583de72e1b
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-21 04:48:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,19 @@
-class Dog
-
-  def name=(dogs_name)
-    @name = dogs_name
+class Person
+  
+  def name=(name)
+    @name = name
   end
-
+  
   def name
     @name
   end
-  def breed=(breed)
-    @breed = breed
-  end
 
-  def breed
-    @breed
-  end
+  def job=(job)
+    @job = job
+  end 
+
+  def job 
+    @job
+  end 
+  
 end
