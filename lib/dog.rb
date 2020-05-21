f2e533b0a47c7399118d48ffb483e44b378b350a
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-21 04:48:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,17 @@
 class Dog
-
-def name=(dog_name)
-  @this_dog_name = dog_name
-end
-
-def name
-  @this_dog_name
-end
-
-def breed=(breed)
-  @breed - breed
-end
-
-def breed
-  @breed
-end
-
+  
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name 
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end 
 end
